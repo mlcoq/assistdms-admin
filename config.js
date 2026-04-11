60a302d1d8669b30fc54e1cfc4657214f390d3e2
Michel le Coq <michel.eindhoven@gmail.com>
2026-04-11 20:06:18 +0200

    Update config.js with Railway production API URL

--- config.js
+++ config.js
@@ -2,12 +2,12 @@
 // Wijzig deze URL naar waar je API draait
 
 const API_CONFIG = {
+    // Production API (Railway hosting):
+    baseUrl: 'https://assistdms-production.up.railway.app'
+
     // Voor lokaal testen (alleen op je eigen machine):
-    // baseUrl: 'https://localhost:5001'
-    
-    // Voor production (vervang met je echte API URL):
-    baseUrl: 'https://jouw-api-url.azurewebsites.net'
-    
+    // baseUrl: 'http://localhost:5001'
+
     // Of gebruik Ngrok voor tijdelijk testen:
     // baseUrl: 'https://abc123.ngrok.io'
 };
